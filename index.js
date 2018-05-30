98e627372f839b2113da6b5d71dcc426952be3a2
Tharles Amaro <tharlesamaro@gmail.com>
2018-05-30 00:55:46 -0300

    acessando a api do google a partir do backend

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var restify = require('restify');
 
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyBzt3sAHIz6d6Md5xexeYDfYyXJ0b3vNwQ',
+    Promise: Promise
+});
+
 const knex = require('knex')({
     client: 'mysql',
     connection: {
@@ -24,6 +29,24 @@ server.get("/all", function (req, res, next) {
         res.send(dados);
     }, next)
     return next();
+});
+
+server.get("/geocode", function (req, res, next) {
+    // Geocode an address with a promise
+    googleMapsClient.geocode({
+            address: '1600 Amphitheatre Parkway, Mountain View, CA'
+        }).asPromise()
+        .then((response) => {
+            const address = response.json.results[0].formatted_address;
+            const place_id = response.json.results[0].place_id;
+            res.send({
+                place_id,
+                address
+            });
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
 server.listen(8080, function () {
