414b9059cfc37c87aee3ede21c2fb63ccf59ac84
Tharles Amaro <tharlesamaro@gmail.com>
2018-05-30 16:19:38 -0300

    salvando os dados no banco de dados. Finalização do servidor REST(backend)

--- index.js
+++ index.js
@@ -44,11 +44,19 @@ server.post("/geocode", function (req, res, next) {
             const address = response.json.results[0].formatted_address;
             const place_id = response.json.results[0].place_id;
             const image = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=10&size=300x300&sensor=false`;
-            res.send({
-                place_id,
-                address,
-                image
-            });
+
+            knex('places')
+                .insert({
+                    place_id,
+                    address,
+                    image
+                })
+                .then(() => {
+                    res.send({
+                        address,
+                        image
+                    });
+                }, next)
         })
         .catch((err) => {
             res.send(err);
