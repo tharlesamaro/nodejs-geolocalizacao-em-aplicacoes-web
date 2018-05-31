3f36692876f37d15f297e89cb5e9f0258cae6770
Tharles Amaro <tharlesamaro@gmail.com>
2018-05-31 15:22:31 -0300

    finalização do sistema

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-var restify = require('restify');
+const restify = require("restify");
+var fs = require('fs');
 
 const googleMapsClient = require('@google/maps').createClient({
     key: 'AIzaSyBzt3sAHIz6d6Md5xexeYDfYyXJ0b3vNwQ',
     Promise: Promise
 });
-
 const knex = require('knex')({
     client: 'mysql',
     connection: {
         host: '127.0.0.1',
         user: 'tharles',
         password: '12345678',
-        database: 'nodejs_geo'
+        database: 'nodejs_js'
     }
 });
 
+
 const server = restify.createServer({
-    name: 'myapp',
-    version: '1.0.0'
+    name: "myapp",
+    version: "1.0.0",
+    key: fs.readFileSync('./key.pem'), //on current folder
+    certificate: fs.readFileSync('./cert.pem')
 });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
@@ -25,9 +28,11 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 server.get("/all", function (req, res, next) {
+
     knex('places').then((dados) => {
         res.send(dados);
     }, next)
+
     return next();
 });
 
@@ -36,14 +41,14 @@ server.post("/geocode", function (req, res, next) {
         lat,
         lng
     } = req.body
-    // Geocode an address with a promise
+
     googleMapsClient.reverseGeocode({
             latlng: [lat, lng]
         }).asPromise()
         .then((response) => {
-            const address = response.json.results[0].formatted_address;
+            const address = response.json.results[0].formatted_address
             const place_id = response.json.results[0].place_id;
-            const image = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=10&size=300x300&sensor=false`;
+            const image = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=12&size=300x300&sensor=false`;
 
             knex('places')
                 .insert({
@@ -57,12 +62,19 @@ server.post("/geocode", function (req, res, next) {
                         image
                     });
                 }, next)
+
         })
         .catch((err) => {
             res.send(err);
         });
+
 });
 
+server.get(/\/(.*)?.*/, restify.plugins.serveStatic({
+    directory: './dist',
+    default: 'index.html',
+}));
+
 server.listen(8080, function () {
-    console.log('%s listening at %s', server.name, server.url);
+    console.log("%s listening at %s", server.name, server.url);
 });
